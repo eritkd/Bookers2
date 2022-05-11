95cefb83da9109c6a8760f7d217b8880f724dcf3
eritkd <102778281+eritkd@users.noreply.github.com>
2022-05-11 06:31:39 +0000

    [Add] 4-課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/show'
+  
+  resources :books, only: [:new,:index,:show]
+  
+  root to: "homes#top"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
