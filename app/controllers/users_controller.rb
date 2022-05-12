c7862af39ec0d414a9b7e35719cd7d4f21fdd886
eritkd <102778281+eritkd@users.noreply.github.com>
2022-05-12 11:42:50 +0000

    [Add] 4-課題

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+
   end
+  
+  private
+
+  def user_params
+   params.require(:user).permit(:name,:profile_image)
+  end
+
+  
 end
